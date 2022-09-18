5daef5c762ddb14286028e787d6534adc4bdc282
andreapmacedo <aa.andremacedo@gmail.com>
2022-09-18 19:36:25 -0300

    feat:(style) implements free shipping icon in the card category.

--- src/components/CategoryCard/index.jsx
+++ src/components/CategoryCard/index.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+// import { ShoppingCart } from 'phosphor-react';
+
 import './style.css';
+
+import freeShippingSvg from '../../assets/free5.svg';
 
 class CategoryCard extends Component {
   render() {
     const { productId, thumbnail, title, price, onClick, shipping } = this.props;
     return (
       <div className="card" >
+        <div className="free-shipping-svg">
+        { shipping && <img src={ freeShippingSvg } alt="Profile" /> }          
+        </div>
+
         <Link
           to={ `/product/${productId}` }
           className="card-link"
@@ -22,8 +30,12 @@ class CategoryCard extends Component {
               {' '}
               {`R$ ${price}`}
             </p>
+            {/* <ShoppingCart size={36} color="#ddd" /> */}
+            {/* <div className="free-shipping">
             { shipping && <p className="shipping" data-testid="free-shipping"> Frete Grátis </p> }
+            </div> */}
           </div>
+          
         </Link>
         <div className="content">
           <input
