6e3ddc009900e8f987d6080efa749166570539ac
andreapmacedo <aa.andremacedo@gmail.com>
2022-09-19 19:25:51 -0300

    feat:(top menu) adiciona input de pesquisa

--- src/components/TopMenu/index.jsx
+++ src/components/TopMenu/index.jsx
@@ -1,19 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import './style.css';
 import MainContext from '../../Context/MainContext';
-import { ShoppingCart } from 'phosphor-react';
+import { ShoppingCart, MagnifyingGlass } from 'phosphor-react';
 
 const TopMenu = () => {
-
+  const [searched, setSearched] = useState('');
   const {
     cartItems
   } = useContext(MainContext);
+
+  const inputHandleChange = ({ target }) => {
+    const { value } = target;
+    setSearched(value);
+  };
 
   return (
     <div className="top-menu-container" >
       <div className="cart-logo">
         <h1>Shopping Cart</h1>
       </div>
+
+      <div className="search-bar">
+        <div>
+          <input
+            className="search-input"
+            autoComplete="off"
+            // autoComplete="false"
+            onChange={ inputHandleChange }
+            value={ searched }
+            name="search-input"
+            type="text"
+            placeholder="search"
+          />          
+        </div>
+        <div className="search-icon" >
+          <MagnifyingGlass size={32} color="#777" />
+        </div>
+      </div>
+
       <div
         className="cart-icon-container"
       >
