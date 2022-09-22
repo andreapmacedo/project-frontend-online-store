3cca8b3f081ce2393c5f8abd9d4203450054dd97
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2022-09-22 16:23:41 -0300

    feat: Adiciona dialog de mansagem ao esgotar a quantidade de um produto

--- src/components/CategoriesCards/index.js
+++ src/components/CategoriesCards/index.js
@@ -3,9 +3,11 @@ import { itemCategory } from '../../services/api';
 import './style.css';
 import MainContext from '../../Context/MainContext'
 import CategoryCard from '../CategoryCard';
+import { Info } from "phosphor-react";
 
 export default function CategoriesCards() {
   const [listitems, setlistitems] = useState([]);
+
   const {
     selectedListItems,
     addToCart,
@@ -33,7 +35,7 @@ export default function CategoriesCards() {
       if (quantity < item.available_quantity) {    
         addToCart(item);
       } else {
-        // TODO: show max quantity message
+        openDialog();
       }
     }
 
@@ -57,18 +59,37 @@ export default function CategoriesCards() {
   // Este useEffect é responsável atualizar a lista de itens toda vez que o context for atualizado.
   useEffect(() => {
     async function getitems(){
-      // const result = await itemCategory('MLB5672');
       const result = await itemCategory(selectedListItems);
-      console.log('result', result);
-      // console.log('result', result);
       setlistitems(result);
     }
     getitems();
   }, [selectedListItems]);
-    
+
+  // Função responsável por abrir o dialog de alerta ao esgotar um produto
+  function openDialog() {
+    // setClassIsShowDialog('dialogOn');
+    const modal = document.querySelector('.modal');
+    modal.classList.add("active");
+  }
+
+  // Função responsável por fechar o dialog de alerta
+  function closeDialog() {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove("active");
+  }
+
   return (
     <div className="main-cards-container">
       {renderItems()}
+      <div className="modal"  onClick={ closeDialog } >
+        <div className="modal-content">
+          <span className="close" onClick={ closeDialog }>&times;</span>
+          <div className="message">
+            <Info size={32} />
+            <p>A quantidade de itens disponíves pelo vendendor se esgotou!</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
